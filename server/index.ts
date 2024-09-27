b855139c180312de0d6d06b8ae70ba0747da8dd6
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-09-27 21:17:53 +0300

    Убрал express

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,7 @@
 import {Socket, Server} from "socket.io";
 import http from "http"
-import cors from "cors"
 import {GameState, GameStateEnum, Player} from "./types";
 import {emptySquares, SERVER_PORT} from "./consts/consts";
-
-const express = require("express");
-const app = express()
-
-app.use(cors())
 
 const server = http.createServer()
 
@@ -109,7 +103,6 @@ io.on("connection", (socket:Socket) => {
 		});
 	})
 })
-
 
 server.listen(SERVER_PORT, () => {
 	console.log("SERVER IS RUNNING")
